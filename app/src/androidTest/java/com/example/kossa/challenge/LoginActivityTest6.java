a8bf64f500402a06846f8e987f47787ff8b2e3a5
gabrielle kibouka <sidekhate@gmail.com>
2018-03-26 14:29:47 +0200

    test six , connexion reussie . Styve Lioumba

---
+++ app/src/androidTest/java/com/example/kossa/challenge/LoginActivityTest6.java
@@ -0,0 +1,40 @@
+package com.example.kossa.challenge;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class LoginActivityTest6 {
+    private String incorrectEmail="ghost@gmail.com";
+    private String inpasseword="ghost1234";
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityActivityTestRule=new ActivityTestRule<>(LoginActivity.class);
+    private LoginActivity loginActivity=null;
+    @Before
+    public void setUp() throws Exception {
+        loginActivity=activityActivityTestRule.getActivity();
+    }
+    @Test
+    public void testActivity(){
+        assertNotNull(loginActivity.findViewById(R.id.btn_login));
+        onView(withId(R.id.tv_email)).perform(typeText(incorrectEmail));
+        onView(withId(R.id.tv_password)).perform(typeText(inpasseword));
+        onView(withId(R.id.btn_login)).perform(click());
+
+    }
+    @After
+    public void tearDown() throws Exception {
+        loginActivity=null;
+    }
+
+}
