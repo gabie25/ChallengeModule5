6ef001f39cc759902ad2bc63ec69fed8816fe1ad
gabrielle kibouka <sidekhate@gmail.com>
2018-03-26 14:21:55 +0200

    test cinq , connexion reussie . Styve Lioumba

---
+++ app/src/androidTest/java/com/example/kossa/challenge/LoginActivityTest5.java
@@ -0,0 +1,41 @@
+package com.example.kossa.challenge;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+
+public class LoginActivityTest5 {
+    private String correctEmail="ghost@gmail.com";
+    private String passeword="ghost123";
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityActivityTestRule=new ActivityTestRule<>(LoginActivity.class);
+    private LoginActivity loginActivity=null;
+    @Before
+    public void setUp() throws Exception {
+        loginActivity=activityActivityTestRule.getActivity();
+    }
+    @Test
+    public void testActivity(){
+        assertNotNull(loginActivity.findViewById(R.id.btn_login));
+        onView(withId(R.id.tv_email)).perform(typeText(correctEmail));
+        onView(withId(R.id.tv_password)).perform(typeText(passeword));
+        onView(withId(R.id.btn_login)).perform(click());
+
+    }
+    @After
+    public void tearDown() throws Exception {
+        loginActivity=null;
+    }
+
+}
